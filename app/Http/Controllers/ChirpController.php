959c2b80c1be1f515906bb616ed9c8016f9534cb
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-09 11:37:25 -0400

    Edit/Update chirp logic

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -55,8 +56,16 @@ class ChirpController extends Controller {
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp) {
-        //
+    public function update(Request $request, Chirp $chirp): RedirectResponse {
+        Gate::authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
